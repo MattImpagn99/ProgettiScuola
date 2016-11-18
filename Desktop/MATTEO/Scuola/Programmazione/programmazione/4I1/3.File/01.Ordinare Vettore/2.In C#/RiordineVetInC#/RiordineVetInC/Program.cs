51df998055fa785534bcdd18ce65cc5da2ea1c98
matteo impagnatiello <matteo impagnatiello>
2016-11-18 13:23:22 +0100

    il mio terzo commit da pc

---
+++ Desktop/MATTEO/Scuola/Programmazione/programmazione/4I1/3.File/01.Ordinare Vettore/2.In C#/RiordineVetInC#/RiordineVetInC/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace RiordineVetInC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vettore generato casualmente");
+            Thread.Sleep(1500);
+            int[] intVetArray = new int[8];
+            Random r = new Random();
+            for(int i=0;i<intVetArray.Length;i++)
+            {
+                intVetArray[i] = r.Next(1, 15);                
+                Console.WriteLine(intVetArray[i]);
+            }
+            Array.Sort(intVetArray);
+            Console.WriteLine(" ");
+            Console.WriteLine("Ecco il vettore riordinato: ");
+            Thread.Sleep(1000);
+            for(int i=0;i<intVetArray.Length;i++)
+            {
+                Console.WriteLine(intVetArray[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
